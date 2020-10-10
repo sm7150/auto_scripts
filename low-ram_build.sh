07bf46c543a3752526bec2b2c38d0a1c4f1c08db
John Car Lo <33212010+firemax13@users.noreply.github.com>
2020-10-10 14:45:32 +0800

    Lineage_Build-Script: Import and Create for LOW RAM
    - Don't do ccache
    - Good for LOW RAM Compile

---
+++ low-ram_build.sh
@@ -0,0 +1,43 @@
+#!bin/bash
+
+echo " " " " " "
+echo "#######################################"
+echo "Auto-Builder Script for Lineage Compile"
+echo "#######################################"
+echo " Copyright 2020 - firemax13@github.com "
+echo "#######################################"
+echo "Auto-Script Builder for Low-RAM Compile"
+echo "#######################################"
+echo " " " " " "
+
+set -e 
+
+echo " " " " "Exporting Some Useful Tools" " "
+# Export Some Java Size Tools & Other things
+export JAVA_TOOL_OPTIONS=-Xmx6g
+export JAVA_TOOL_OPTIONS=-Xmx6g
+
+echo " " " " "Unexporting Some Useless Tools" " "
+# Unexport CCACHE. CCACHE will slow down build & make some errors if dirty building
+unexport USE_CCACHE=1
+unexport USE_CCACHE=1
+unexport CCACHE_EXEC=/usr/bin/ccache
+unexport CCACHE_EXEC=/usr/bin/ccache
+
+# Device Codenames & other settings
+DEVICE_CODE="a70q"
+
+# Do initialize 
+. build/envsetup.sh
+
+# Lunch Device
+lunch lineage_$DEVICE_CODE-userdebug
+
+# Start Building for JAVA Docs first to prevent any further stoppage of errors for Low-RAM
+mka api-stubs-docs
+mka hiddenapi-lists-docs
+mka system-api-stubs-docs
+mka test-api-stubs-docs
+
+# Brunch Device
+brunch lineage_$DEVICE_CODE-userdebug
