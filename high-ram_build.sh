d16ceac347c368d9aaf9115575759815d7333646
John Car Lo <33212010+firemax13@users.noreply.github.com>
2020-10-13 08:32:55 +0800

    HighRamBuild: Don't Export and Clean Build
    - Add notes for low ram area

--- high-ram_build.sh
+++ high-ram_build.sh
@@ -12,17 +12,14 @@ echo " " " " " "
 
 set -e 
 
+# Do Clean Build Else Die
+rm -rf out
+rm -rf out
+
 echo " " " " "Exporting Some Useful Tools" " "
 # Export Some Java Size Tools & Other things
 export JAVA_TOOL_OPTIONS=-Xmx8g
 export JAVA_TOOL_OPTIONS=-Xmx8g
-
-echo " " " " "Unexporting Some Useless Tools" " "
-# Unexport CCACHE. CCACHE will slow down build & make some errors if dirty building
-unexport USE_CCACHE=1
-unexport USE_CCACHE=1
-unexport CCACHE_EXEC=/usr/bin/ccache
-unexport CCACHE_EXEC=/usr/bin/ccache
 
 # Device Codenames & other settings
 DEVICE_CODE="a70q"
@@ -33,6 +30,7 @@ DEVICE_CODE="a70q"
 # Lunch Device
 lunch lineage_$DEVICE_CODE-userdebug
 
+# ONLY FOR LOW RAM, CHECK THE OTHER SCRIPT FOR LOW RAM COMPILE
 ############################################################################################
 # Start Building for JAVA Docs first to prevent any further stoppage of errors for Low-RAM #
 # mka api-stubs-docs                                                                       #
